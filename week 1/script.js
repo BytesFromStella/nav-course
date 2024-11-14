471ebb6af10078368e19492989aaa690ddd0d7f9
BytesFromStella <steffentvenningj@gmail.com>
2024-11-14 09:01:05 +0100

    Created resizing functions for the container object

--- week 1/script.js
+++ week 1/script.js
@@ -12,12 +12,34 @@ function alertButton() {
     alert(parseInt(sum))
 }
 
-function resizeTarget(targetCSS) { // Reusable pointer to CSS code for body WIDTH, acting as a resizer
-    document.documentElement.style.setProperty("bodySizeAdjust", parseint(targetCSS)+"%") // Parsing as int to prevent invalid strings/properties.
+
+function resizeTarget(targetObject, targetProperty, resizeInt) { // Reusable pointer to CSS code for body WIDTH, acting as a resizer
+    const container = document.getElementById(`${targetObject}`)
+    try {
+        parseInt(resizeInt);
+        toString(targetProperty);
+        toString(targetObject);
+    }
+    catch(e) {
+        console.error("This isn't a number, or something broke. Try again.")
+    }
+    while (Number.isInteger(resizeInt)) {
+        container.targetProperty = toString(resizeInt)+"%" // Parsing as int to prevent invalid strings/properties.
+    }
 }
 
-function sizeUpButton() {
-    resizeTarget(90)
-}
 
-document.getElementById()
+function defaultSize() {
+    resizeTarget("container", "width", 50);
+}
+document.getElementById("sizeResetButton").addEventListener("click", defaultSize());
+
+function sizeUp() {
+    resizeTarget("container", "width", 80)
+}
+document.getElementById("sizeUpButton").addEventListener("click", sizeUpButton());
+
+function sizeDown() {
+    resizeTarget("container", "width", 30)
+}
+document.getElementById("sizeDownButton").addEventListener("click", sizeDownButton());
